bca353f99dd35015a1f85130859c59bc9d1141f0
Cydeamon <Cydeamon@gmail.com>
2022-08-09 23:00:36 +0300

    Animated background. Menu. Working menu options

---
+++ src/Main.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+func _ready():
+	pass # Replace with function body.
+
+func _on_menu_options_item_activated(index:int):
+	print("Selected: " + $Menu/menu_options.get_item_text(index))
+	var itemText = $Menu/menu_options.get_item_text(index)
+	
+	if itemText == "Start game":
+		start_game()
+	elif itemText == "Settings":
+		pass
+	elif itemText == "Exit":
+		get_tree().quit()
+
+func start_game():
+	$Menu.visible = false
